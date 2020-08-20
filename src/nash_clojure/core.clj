357eee371b22ae87a95cdcdf6bb7fff1fabafd8b
tom <tom@that-place>
2020-08-20 02:19:17 -0500

    117x fold speed improvement, ok memory.

--- src/nash_clojure/core.clj
+++ src/nash_clojure/core.clj
@@ -20,6 +20,7 @@
   [& vectors]
   (apply map bit-and vectors))
 
+;;no primitive math, boxed.
 (defn one-index
   "Given a player-agnostic strategy index, find the index of the 1 for the given row." 
   [row-selector number-of-columns strategy-index]
@@ -27,17 +28,21 @@
      (mod (quot strategy-index (expt number-of-columns row-selector))
           number-of-columns)))
 
+;;intermediate seqs, not necessary if we just care about sparse indices.
 (defn one-indexes
   "Given a player-agnostic strategy index, find the index of the 1 for each row."
   [number-of-rows number-of-columns strategy-index]
   (for [row-selector (range number-of-rows)]
     (one-index row-selector number-of-columns strategy-index)))
 
+;;possible bottleneck too.  looks like it could be memoized too.
+;;creates new vectors needlessly, tosses them for seqs.  hmm.
+
 (defn player-agnostic-strategy
   "Given a strategy index, find the player-agnostics strategy for a game of the given size."
   [number-of-rows number-of-columns strategy-index]
   (apply vector-bit-or (map #(assoc (vec (repeat (* number-of-rows number-of-columns) 0)) % 1)
-                             (one-indexes number-of-rows number-of-columns strategy-index))))
+                            (one-indexes number-of-rows number-of-columns strategy-index))))
 
 ;;very slow due to apply map vector...
 (defn column-rank-order
@@ -58,7 +63,6 @@
   "Find the player 2 strategy for the given strategy index."
   [number-of-rows number-of-columns strategy-index]
   (player-agnostic-strategy number-of-rows number-of-columns strategy-index))
-
 ;
 ; Nash game categorization
 ;
@@ -69,6 +73,7 @@
   [player-one-strategy player-two-strategy]
   (count (take 2 (filter #{1} (vector-bit-and player-one-strategy player-two-strategy)))))
 
+
 (defn categorize-nash-game
   "Generate the player 1 and 2 strategies and categorize the Nash solutions for the given game index."
   [number-of-rows number-of-columns game-index]
@@ -77,22 +82,11 @@
                             (player-two-strategy number-of-rows number-of-columns
                                                  (mod game-index (expt number-of-rows number-of-columns)))))
 
+;;fine
 (defn number-of-nash-games
   "Determine the number of Nash games of a given size"
   [number-of-rows number-of-columns]
   (* (expt number-of-rows number-of-columns) (expt number-of-columns number-of-rows)))
-
-(defn nash-game-partitions [entries-per-partition number-of-games]
-  (map #(conj [%] (+ % (- entries-per-partition 1)))
-       (filter #(and (>= (- number-of-games %) entries-per-partition)
-                     (= 0 (mod % entries-per-partition)))
-               (range number-of-games))))
-
-(defn nash-game-partitions [entries-per-partition number-of-games]
-  (for [n (->>(range number-of-games)
-              (filter #(and (>= (- number-of-games %) entries-per-partition)
-                            (= 0 (mod % entries-per-partition)))))]
-    [n (+ n (- entries-per-partition 1))]))
 
 ;;not good.
 #_(defn partition-nash-games
@@ -122,6 +116,7 @@
          (== parts 1)
          [[l r]])))
 
+;;should be instant.
 (defn partition-nash-games
   "Determine the start and end indices of each group of Nash games"
   [number-of-partitions number-of-games]
@@ -151,7 +146,7 @@
     (into (sorted-map) (reduce #(merge-with + %1 %2)
                                (pmap #(categorize-given-nash-games number-of-rows number-of-columns %)
                                      (partition-nash-games number-of-partitions number-of-games))))))
-;
+
 ; Main function
 ;
 
@@ -171,7 +166,41 @@
 
 
 (comment
+
+  ;;about 117x faster now to do a game.
+
+  ;; nash-clojure.core> (c/quick-bench (categorize-nash-game 5 5 100))
+  ;; Evaluation count : 2838 in 6 samples of 473 calls.
+  ;; Execution time mean : 210.810692 µs
+  ;; Execution time std-deviation : 6.454449 µs
+  ;; Execution time lower quantile : 204.950579 µs ( 2.5%)
+  ;; Execution time upper quantile : 220.192915 µs (97.5%)
+  ;; Overhead used : 1.852568 ns
+  ;; nil
+
+  ;; nash-clojure.core> (c/quick-bench (categorize-nash-game-fast 5 5 100))
+  ;; Evaluation count : 347490 in 6 samples of 57915 calls.
+  ;; Execution time mean : 1.782560 µs
+  ;; Execution time std-deviation : 55.777733 ns
+  ;; Execution time lower quantile : 1.720418 µs ( 2.5%)
+  ;; Execution time upper quantile : 1.838751 µs (97.5%)
+  ;; Overhead used : 1.852568 ns
+  ;; nil
+
+
+  ;;Same results over 100K random games.  Looking good!
+
+  ;; nash-clojure.core> (->>  (repeatedly 100000 #(rand-int (number-of-nash-games 5 5)))
+  ;;                          (map (fn [idx]  [idx (categorize-nash-game 5 5 idx) (categorize-nash-game-fast 5 5 idx)]))
+  ;;                          (some (fn [[idx l r]] (when (not= l r) idx))))
+  ;; nil
+
+  ;;took ~11 minutes wit 4 threads.
   (defn test [& {:keys [n] :or {n 1}}]
     (println (time (pcategorize-nash-games 5 5 n))))
+
+  ;;Done in 6 seconds on 4 threads.
+  (defn test-fast [& {:keys [n] :or {n 1}}]
+    (println (time (pcategorize-nash-games-fast 5 5 n))))
 
   )
---
+++ src/nash_clojure/fast.clj
@@ -0,0 +1,192 @@
+(ns nash-clojure.fast
+  (:require [nash-clojure.core :as core]))
+
+;;Faster path using primitive arrays and direct indexing.
+(set! *unchecked-math* true)
+
+(defn one-index-fast
+  "Given a player-agnostic strategy index, find the index of the 1 for the given row."
+  ^long [^long row-selector ^long number-of-columns ^long strategy-index]
+  (+ (* row-selector number-of-columns)
+     (mod (quot strategy-index (Math/pow number-of-columns row-selector))
+          number-of-columns)))
+
+;;eliminates intermediate seq; use native ctor since clojure.core/eduction ends up
+;;invoking a bunch of seq stuff just to compose the functions passed to it; we
+;;only have 2 inputs so can directly build the thing.
+(defn one-indexes-fast
+  "Given a player-agnostic strategy index, find the index of the 1 for each row."
+  [number-of-rows number-of-columns strategy-index]
+  (clojure.core.Eduction.
+   (map #(one-index-fast % number-of-columns strategy-index))
+   (range number-of-rows)))
+
+;;only or's where we know there are values.  eliminates intermediate
+;;vectors/seqs, sparse traversal.
+(defn or-ones [rows columns indices]
+  (reduce (fn [^longs arr idx]
+            (do (aset arr idx 1)
+                arr))
+          (long-array (* ^long rows ^long columns)) indices))
+
+;;no need to delineate if we have no need for column-rank-order..
+;;~5-6x faster
+(defn player-agnostic-strategy-fast
+  "Given a strategy index, find the player-agnostics strategy for a game of the given size."
+  [number-of-rows number-of-columns strategy-index]
+  (or-ones number-of-rows number-of-columns
+           (one-indexes-fast number-of-rows number-of-columns strategy-index)))
+
+;;~57x faster
+(defn row-col-and [^longs row-major ^longs col-major rows cols]
+  (let [result     (long-array  (* ^long rows ^long cols))
+        n          (volatile! 0)]
+    (dotimes [i rows]
+      (dotimes [j cols]
+        (let [^long idx @n]
+          (aset result ^long idx ^long (bit-and (aget row-major (+ (* j rows) i))
+                                                (aget col-major idx)))
+          (vreset! n (unchecked-inc idx)))))
+    result))
+(set! *unchecked-math* false)
+
+(defn categorize-nash-solution-fast
+  "Categorize the Nash solutions for the given two player game."
+  [player-one-strategy player-two-strategy rows cols]
+  (let [^longs arr (row-col-and player-one-strategy player-two-strategy rows cols)]
+    (->> (areduce arr idx acc 0 (if (zero? (aget arr idx))
+                             acc
+                             (unchecked-inc acc)))
+         (min 2)))) ;;replaces the old filter for 1's, take 2, count from before.
+
+;;about 100x faster.
+(defn categorize-nash-game-fast
+  "Generate the player 1 and 2 strategies and categorize the Nash solutions for the given game index."
+  [number-of-rows number-of-columns game-index]
+  (categorize-nash-solution-fast
+   (player-agnostic-strategy-fast number-of-rows number-of-columns
+      (quot game-index (Math/pow ^long number-of-rows ^long number-of-columns)))
+   (player-agnostic-strategy-fast number-of-rows number-of-columns
+                                  (mod game-index (Math/pow  ^long number-of-rows ^long number-of-columns)))
+   number-of-rows number-of-columns))
+
+
+
+;;Fast path.
+
+(defn categorize-given-nash-games-fast
+  "Categorize the Nash solutions for games with the given games indoces."
+  [number-of-rows number-of-columns start-and-end-indices]
+  (frequencies (map #(categorize-nash-game-fast number-of-rows number-of-columns %)
+                    (range (nth start-and-end-indices 0) (+ (nth start-and-end-indices 1) 1)))))
+
+(defn categorize-nash-games-fast
+  "Categorize the Nash solutions for games of a given size, using the given number of paritions
+   using one thread."
+  [number-of-rows number-of-columns number-of-partitions]
+  (let [number-of-games (core/number-of-nash-games number-of-rows number-of-columns)]
+    (into (sorted-map) (reduce #(merge-with + %1 %2) 
+                               (map #(categorize-given-nash-games-fast number-of-rows number-of-columns %)
+                                    (core/partition-nash-games number-of-partitions number-of-games))))))
+
+(defn pcategorize-nash-games-fast
+  "Categorize the Nash solutions for games of a given size, using the given number of partitions
+   using one thread per partition."
+  [number-of-rows number-of-columns number-of-partitions]
+  (let [number-of-games (core/number-of-nash-games number-of-rows number-of-columns)]
+    (into (sorted-map) (reduce #(merge-with + %1 %2)
+                               (pmap #(categorize-given-nash-games-fast number-of-rows number-of-columns %)
+                                     (core/partition-nash-games number-of-partitions number-of-games))))))
+
+
+(set! *unchecked-math* false)
+
+;;took ~11 minutes wit 4 threads.
+(defn bench [& {:keys [rows cols n] :or {rows 5 cols 5 n 1}}]
+  (println (time (core/pcategorize-nash-games rows cols n))))
+
+;;Done in 6 seconds on 4 threads.
+(defn bench-fast [& {:keys [rows cols n] :or {rows 5 cols 5 n 1}}]
+  (println (time (pcategorize-nash-games-fast rows cols n))))
+
+
+(comment
+
+  ;;about 117x faster now to do a game.
+
+  ;; nash-clojure.core> (c/quick-bench (core/categorize-nash-game 5 5 100))
+  ;; Evaluation count : 2838 in 6 samples of 473 calls.
+  ;; Execution time mean : 210.810692 µs
+  ;; Execution time std-deviation : 6.454449 µs
+  ;; Execution time lower quantile : 204.950579 µs ( 2.5%)
+  ;; Execution time upper quantile : 220.192915 µs (97.5%)
+  ;; Overhead used : 1.852568 ns
+  ;; nil
+
+  ;; nash-clojure.core> (c/quick-bench (categorize-nash-game-fast 5 5 100))
+  ;; Evaluation count : 347490 in 6 samples of 57915 calls.
+  ;; Execution time mean : 1.782560 µs
+  ;; Execution time std-deviation : 55.777733 ns
+  ;; Execution time lower quantile : 1.720418 µs ( 2.5%)
+  ;; Execution time upper quantile : 1.838751 µs (97.5%)
+  ;; Overhead used : 1.852568 ns
+  ;; nil
+
+
+  ;;Same results over 100K random games.  Looking good!
+
+  ;; nash-clojure.core> (->>  (repeatedly 100000 #(rand-int (core/number-of-nash-games 5 5)))
+  ;;                          (map (fn [idx]  [idx (core/categorize-nash-game 5 5 idx) (categorize-nash-game-fast 5 5 idx)]))
+  ;;                          (some (fn [[idx l r]] (when (not= l r) idx))))
+  ;; nil
+
+
+  ;; So we get - as expected - about linear speedup on my
+  ;; rig up through 4x.
+
+  ;; After that, the dminishing returns start kicking in.
+  ;; I have 4 logical cores, 8 "processors" likely hyper threads on mine.
+
+  ;;This is typical scaling behavior I've observed with Clojure.
+
+  ;; nash-clojure.fast> (bench-fast :n 1)
+  ;; "Elapsed time: 18339.8527 msecs"
+  ;; {0 2764880, 1 4581226, 2 2419520}
+  ;; nil
+  ;; nash-clojure.fast> (bench-fast :n 2)
+  ;; "Elapsed time: 9959.0236 msecs"
+  ;; {0 2764880, 1 4581225, 2 2419520}
+  ;; nil
+  ;; nash-clojure.fast> (bench-fast :n 3)
+  ;; "Elapsed time: 7139.6892 msecs"
+  ;; {0 2764880, 1 4581225, 2 2419520}
+  ;; nil
+  ;; nash-clojure.fast> (bench-fast :n 4)
+  ;; "Elapsed time: 5855.4898 msecs"
+  ;; {0 2764880, 1 4581225, 2 2419520}
+  ;; nil
+  ;; nash-clojure.fast> (bench-fast :n 5)
+  ;; "Elapsed time: 5070.2283 msecs"
+  ;; {0 2764880, 1 4581225, 2 2419520}
+  ;; nil
+  ;; nash-clojure.fast> (bench-fast :n 6)
+  ;; "Elapsed time: 4530.2396 msecs"
+  ;; {0 2764880, 1 4581225, 2 2419520}
+  ;; nil
+  ;; nash-clojure.fast> (bench-fast :n 7)
+  ;; "Elapsed time: 4143.6082 msecs"
+  ;; {0 2764880, 1 4581225, 2 2419520}
+  ;; nil
+  ;; nash-clojure.fast> (bench-fast :n 8)
+  ;; "Elapsed time: 3958.4207 msecs"
+  ;; {0 2764880, 1 4581225, 2 2419520}
+  ;; nil
+  ;; nash-clojure.fast> (bench-fast :n 9)
+  ;; "Elapsed time: 3927.3654 msecs"
+  ;; {0 2764880, 1 4581225, 2 2419520}
+  ;; nil
+  ;; nash-clojure.fast> (bench-fast :n 10)
+  ;; "Elapsed time: 3937.4671 msecs"
+  ;; {0 2764880, 1 4581225, 2 2419520}
+  ;; nil
+  )
